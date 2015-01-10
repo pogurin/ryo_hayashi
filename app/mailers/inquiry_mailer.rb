5cfe9dc03b522ee690d15a91079cf92715244144
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-10 17:25:19 -0500

    suceed sending email by using localhost

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,11 +1,9 @@
 class InquiryMailer < ActionMailer::Base
   default from: "seiun340066@gmail.com"   # 送信元アドレス
-  # default to: "seiun340066@gmail.com"     # 送信先アドレス
- 
-  def sendmail_confirm
-     @greeting = "Hi"
-
-    mail to: "seiun340066@gmail.com", subject: "ActionMailer test"
+  default to: "seiun340066@gmail.com" 
+  
+ def received_email(inquiry)
+    @inquiry = inquiry
+    mail(:subject => 'お問い合わせを承りました')
   end
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources :co_steps 
   resources :works
   resources :missions
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
 end
+
+
