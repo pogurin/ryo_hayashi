ff02d6934b98b8a016b10d34c5a9884f2ab23242
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-10 20:49:53 -0500

    modal fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -87,18 +87,52 @@ $(document).ready(function() {
 });
 
 
+
+
 $(function(){
-	$('.btns').click(function(){
-		wn = '.' + $(this).data('tgt');
-		var mW = $(wn).find('.modalBody').innerWidth() / 2;
-		var mH = $(wn).find('.modalBody').innerHeight() / 2;
-		$(wn).find('.modalBody').css({'margin-left':-mW,'margin-top':-mH});
-		$(wn).fadeIn(500);
-	});
-	$('.close,.modalBK').click(function(){
-		$(wn).fadeOut(500);
-	});
+    // 「.modal-open」をクリック
+    $('.modal-open').click(function(){
+        // オーバーレイ用の要素を追加
+        $('body').append('<div class="modal-overlay"></div>');
+        // オーバーレイをフェードイン
+        $('.modal-overlay').fadeIn('slow');
+
+        // モーダルコンテンツのIDを取得
+        var modal = '#' + $(this).attr('data-target');
+        // モーダルコンテンツの表示位置を設定
+        modalResize();
+         // モーダルコンテンツフェードイン
+        $(modal).fadeIn('slow');
+
+        // 「.modal-overlay」あるいは「.modal-close」をクリック
+        $('.modal-overlay, .modal-close').off().click(function(){
+            // モーダルコンテンツとオーバーレイをフェードアウト
+            $(modal).fadeOut('slow');
+            $('.modal-overlay').fadeOut('slow',function(){
+                // オーバーレイを削除
+                $('.modal-overlay').remove();
+            });
+        });
+
+        // リサイズしたら表示位置を再取得
+        $(window).on('resize', function(){
+            modalResize();
+        });
+
+        // モーダルコンテンツの表示位置を設定する関数
+        function modalResize(){
+            // ウィンドウの横幅、高さを取得
+            var w = $(window).width();
+            var h = $(window).height();
+
+            // モーダルコンテンツの表示位置を取得
+            var x = (w - $(modal).outerWidth(true)) / 2;
+            var y = (h - $(modal).outerHeight(true)) / 2;
+
+            // モーダルコンテンツの表示位置を設定
+            $(modal).css({'left': x + 'px','top': y + 'px'});
+        }
+
+    });
 });
-
-
 
