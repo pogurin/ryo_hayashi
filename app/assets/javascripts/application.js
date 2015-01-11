dbca4ac4e319f6d53fc846da5ceca359b440ba4b
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-10 21:17:25 -0500

    more mission statement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -136,3 +136,14 @@ $(function(){
     });
 });
 
+
+$(function(){
+$('.content:not(.content:first-of-type)').css('display','none');//一番上の要素以外を非表示
+$('.more').nextAll('.more').css('display','none');//ボタンを非表示
+$('.more').on('click', function() {
+$(this).css('display','none');//押したボタンを非表示
+$(this).next('.content').slideDown('slow');
+$(this).nextAll('.more:first').css('display','block'); //次のボタンを表示
+});
+});
+
