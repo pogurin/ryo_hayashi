5fcd6eb4c60fab353816956a8d31755f8a303527
MitsuyoshiToguri <seiun340066@gmail.com>
2015-05-31 02:45:18 -0400

    First pages commit

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_ryo_hayashi_session'
--- test/controllers/co_steps_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CoStepsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
